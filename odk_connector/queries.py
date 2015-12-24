0527e64896935e6f0025811f94c9aad8d6f5cf3a
Mohamed Gamal <mohamed.gamal@cartologic.com>
2015-12-24 11:26:41 +0200

    update create view query to be updatable
    This modification require PostgreSQL 9.4 or later

--- odk_connector/queries.py
+++ odk_connector/queries.py
@@ -1,1 +1,1 @@
-create_view = 'CREATE OR REPLACE VIEW "%s" AS SELECT *, ST_SetSRID(ST_MakePoint("LOCATION_LNG", "LOCATION_LAT"), 4326) AS the_geom FROM "%s";'
+create_view = 'CREATE OR REPLACE VIEW "%s" AS SELECT *, ST_SetSRID(ST_MakePoint("LOCATION_LNG", "LOCATION_LAT"), 4326) AS the_geom FROM "%s" WITH CHECK OPTION;'
